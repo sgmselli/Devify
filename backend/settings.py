d8c434187861c2a34426a81ef1a4f2fc9f64bf5a
sgmselli <sellings.matthew@gmail.com>
2023-04-16 20:09:53 +0100

    Edit requirements

--- backend/settings.py
+++ backend/settings.py
@@ -47,9 +47,7 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    
 }
 
 SIMPLE_JWT = {
