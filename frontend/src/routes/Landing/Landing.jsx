ade9b4d01ed4e3be0d928f91000e11791ce864d6
sgmselli <sellings.matthew@gmail.com>
2023-05-07 13:57:30 +0100

    freelance resizing

--- frontend/src/routes/Landing/Landing.jsx
+++ frontend/src/routes/Landing/Landing.jsx
@@ -404,10 +404,10 @@ function Landing() {
                 </div>
 
                 <div className='freelanceIndex'>
-                    <div className='row'>
+                    <div className='row freelanceContainer'>
 
-                        <div className='col-lg'>
-                            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                        <div className='col-lg freelanceCol'>
+                            <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
                                         <div className='landingCarousel'>
@@ -469,22 +469,6 @@ function Landing() {
                                         </div>
                                     </div>
                                 </div>
-                                <a
-                                    class="carousel-control-prev"
-                                    href="#carouselExampleControls"
-                                    role="button"
-                                    data-slide="prev">
-                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Previous</span>
-                                </a>
-                                <a
-                                    class="carousel-control-next"
-                                    href="#carouselExampleControls"
-                                    role="button"
-                                    data-slide="next">
-                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                    <span class="sr-only">Next</span>
-                                </a>
                             </div>
                         </div>
 
